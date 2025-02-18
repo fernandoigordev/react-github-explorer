5362375e3c583dd288517b356fda3ac5cca2bec3
Fernando Igor <79016043+fernandoigordev@users.noreply.github.com>
2025-02-18 13:30:00 -0300

    Adicionado consulta na api do github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,25 @@
+import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/Repositories.css";
 
-const repository = {
-    name: "wk-tecnology",
-    description: "Teste desenvolvedor Delphi",
-    link: "#"
-}
-
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(()=>{
+        fetch('https://api.github.com/users/fernandoigordev/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
+    console.log(repositories)
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}    
             </ul>
         </section>
     );
